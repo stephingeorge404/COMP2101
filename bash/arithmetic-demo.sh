ea6a3f74c2b4fd32de42a73937843c85dee8b9d6
stephingeorge404 <stephingeorge404@gmail.com>
2019-02-20 21:19:05 -0500

    Lab 2

---
+++ bash/arithmetic-demo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# this script demonstrates doing arithmetic
+
+# Improve this script by asking the user to supply the two numbers
+# Improve this script by demonstrating using subtraction and multiplication
+# Improve this script by demonstrating the modulus operation
+#   - the output should look something like:
+#   - first divided by second gives X with a remainder of Y
+# Improve this script by calculating and displaying the first number raised to the power of the second number
+
+echo "Enter two integers: "
+
+read -p firstnum: firstnum
+read -p secondnum: secondnum
+sum=$((firstnum + secondnum))
+sub=$((firstnum - secondnum))
+mul=$((firstnum * secondnum))
+mod=$((firstnum % secondnum))
+pow=$((firstnum ** secondnum))
+dividend=$((firstnum / secondnum))
+fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+
+cat <<EOF
+$firstnum plus $secondnum is $sum
+$firstnum minus $secondnum is $sub
+$firstnum multiplied $secondnum is $mul
+$firstnum divided by $secondnum is $dividend
+$firstnum divided by $secondnum gives $dividend with a remainder of $mod
+- More precisely, it is $fpdividend
+$firstnum raised to the power of the $secondnum is $pow
+
+EOF
---
+++ bash/welcome-example.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This script produces the dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Improve this script by using the value in the automatic variable $USER for the name
+# Improve this script by adding some time and day of the week information to the welcome message
+#   Use a format like this:
+#   It is HH:MM AM on weekday.
+
+###############
+# Variables   #
+###############
+title="Overlord"
+myname="stephingeorge"
+hostname=$(hostname)
+NOW=$(date +"%h-%M-%P")
+day=$(date +"%A")
+
+###############
+# Main        #
+###############
+cat <<EOF
+
+Welcome to planet $hostname, "$title $USER!" current time is $NOW on $day
+
+EOF
